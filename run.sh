7285269310f328aaa48e91f7b4266c6b7e449a03
Josua Krause <josua.krause@gmail.com>
2013-11-23 17:25:43 -0500

    let vendor build up

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ cd vendor
 rm manifest.dat vendor.log
 python uppir_create_manifest.py ../filestoshare/ 1024 127.0.0.1
 python uppir_vendor.py --foreground &
+sleep 1
 cd ../mirror1
 rm manifest.dat mirror.log
 python uppir_mirror.py --ip=127.0.0.1 --port 62001 --foreground --mirrorroot=filestoshare/ --httpport=61002 --retrievemanifestfrom=127.0.0.1 &
