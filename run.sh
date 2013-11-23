9706ac655d9bdd999545fdf39ab5c2bc06d99ca4
Josua Krause <josua.krause@gmail.com>
2013-11-23 17:33:06 -0500

    run updated

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ python uppir_vendor.py --foreground &
 sleep 1
 cd ../mirror1
 rm manifest.dat mirror.log
-python uppir_mirror.py --ip=127.0.0.1 --port 62001 --foreground --mirrorroot=filestoshare/ --httpport=61002 --retrievemanifestfrom=127.0.0.1 &
+python uppir_mirror.py --ip=127.0.0.1 --port 62001 --foreground --mirrorroot=../filestoshare/ --httpport=61002 --retrievemanifestfrom=127.0.0.1 &
 cd ../mirror2
 rm manifest.dat mirror.log
-python uppir_mirror.py --ip=127.0.0.1 --port 62002 --foreground --mirrorroot=filestoshare/ --httpport=61002 --retrievemanifestfrom=127.0.0.1 &
+python uppir_mirror.py --ip=127.0.0.1 --port 62002 --foreground --mirrorroot=../filestoshare/ --httpport=61002 --retrievemanifestfrom=127.0.0.1 &
 cd ../mirror3
 rm manifest.dat mirror.log
-python uppir_mirror.py --ip=127.0.0.1 --port 62003 --foreground --mirrorroot=filestoshare/ --httpport=61002 --retrievemanifestfrom=127.0.0.1 &
+python uppir_mirror.py --ip=127.0.0.1 --port 62003 --foreground --mirrorroot=../filestoshare/ --httpport=61002 --retrievemanifestfrom=127.0.0.1 &
 cd ..
